76266bbefa7c30a051ece1e192dde9b700c42450
carol <carolzinha.mgonzalez@gmail.com>
2019-01-26 19:35:58 -0200

    header

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+
+const header = () => {
+  return (
+    <nav className="navbar navbar-expand-sm fixed-top navbar-light" id="nav-color">
+      <div className="container">
+        <a className="navbar-brand icon-user" id="user-color" href="#"> Visitante</a>
+        
+        <div className="dropdown">
+          <a className="btn dropdown-toggle text-color" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Visualizar matérias
+          </a>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a className="dropdown-item" href="#">Todas</a>
+            <a className="dropdown-item" href="#">Personalizadas</a>
+            <a className="dropdown-item" href="#">Gerais</a>
+          </div>
+        </div>
+
+      </div>
+    </nav>
+  )
+};
+
+export default header;
