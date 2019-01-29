2621fcb781163cd4d5969ff3103335c1312c2485
carol <carolzinha.mgonzalez@gmail.com>
2019-01-29 20:00:24 -0200

    style

--- src/containers/GenericMatters.js
+++ src/containers/GenericMatters.js
@@ -5,11 +5,11 @@ import { connect } from 'react-redux';
 class GenericMatters extends Component { 
   render() {
     return (   
-      <div className="container offset-1">
+      <div className="container offset-2">
         <h3> Disciplinas Gerais </h3>
         <div className="row">
           {this.props.base[0].map(n => n.generic === true ? 
-              <div className="col-sm-3 size border b-generic m-1 d-flex justify-content-center flex-row">
+              <div className="col-sm-3 size border b-generic m-1 pt-2 d-flex justify-content-center flex-row">
                 <div className="d-flex align-self-center">  
                   <p className="p-generic font-weight-bold img-generic" key={n.id} > 
                     {n.name} 
--- src/containers/HighlightMatters.js
+++ src/containers/HighlightMatters.js
@@ -5,20 +5,18 @@ import { connect } from 'react-redux';
 class HighlightMatters extends Component { 
   render() {
     return (   
-      <div className="container">
-        <h3> Disciplinas Personalizadas </h3>
-        <div className="d-flex">          
-          <div className="row d-flex align-items-center">
-            {this.props.base[0].map(n => n.highlight === true ? 
-                <div className="col-sm-3 size border b-highlight m-1 d-flex justify-content-center">
-                  <p className="p-highlight font-weight-bold img-generic" key={n.id} > 
-                    {n.name} 
-                  </p>
-                </div>  
-              : <div> </div> 
-            )}              
-          </div>
-        </div> 
+      <div className="container offset-2">
+        <h3> Disciplinas Personalizadas </h3>          
+        <div className="row d-flex">
+          {this.props.base[0].map(n => n.highlight === true ? 
+              <div className="col-sm-3 size border b-highlight m-1 pt-2 d-flex justify-content-center">
+                <p className="p-highlight font-weight-bold img-generic" key={n.id} > 
+                  {n.name} 
+                </p>
+              </div>  
+            : <div> </div> 
+          )}              
+        </div>
       </div>
     )
   }
